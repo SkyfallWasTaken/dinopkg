4d4223acdc1c913842fb7ef9c744636e4bb57ca9
SkyfallWasTaken <mahadkalam1@proton.me>
2024-07-09 20:49:26 +0100

    Add fields to package.json

--- crates/dinopkg-package-json/src/main.rs
+++ crates/dinopkg-package-json/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct PackageJson {
+    pub name: String,
+    pub version: String,
+    pub description: Option<String>,
+    pub main: Option<String>,
+    pub scripts: Option<Scripts>,
+    pub dependencies: Option<Dependencies>,
+    pub dev_dependencies: Option<Dependencies>,
+}
+
+pub type Scripts = HashMap<String, String>;
+pub type Dependencies = HashMap<String, String>;
+
+impl PackageJson {
+    pub fn parse(json: &str) -> Result<Self, serde_json::Error> {
+        serde_json::from_str(json)
+    }
 }

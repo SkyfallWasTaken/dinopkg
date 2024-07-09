4db902d286f34ceda04b0b03aa77982ef2aa544f
SkyfallWasTaken <mahadkalam1@proton.me>
2024-07-09 21:39:58 +0100

    Start run command

--- crates/dinopkg-cli/src/command/run.rs
+++ crates/dinopkg-cli/src/command/run.rs
@@ -1,8 +1,26 @@
-use color_eyre::Result;
+use color_eyre::{eyre::eyre, Result};
 use dinopkg_package_json::PackageJson;
+use owo_colors::OwoColorize;
 
 pub async fn run(script_name: Option<String>) -> Result<()> {
     let package_json = PackageJson::from_file(10).await?;
-    println!("Running script: {:?} {}", script_name, package_json.name);
+
+    match script_name {
+        Some(script_name) => {
+            let Some(scripts) = package_json.scripts else {
+                return Err(eyre!("no `scripts` provided in package.json"));
+            };
+            match scripts.get(&script_name) {
+                Some(script) => {
+                    log::info!("{script_name}: {script}")
+                }
+                _ => return Err(eyre!(format!("script `{script_name}` not found"))),
+            }
+        }
+        _ => {
+            todo!()
+        }
+    }
+
     Ok(())
 }
--- crates/dinopkg-cli/src/main.rs
+++ crates/dinopkg-cli/src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use env_logger::{Builder, Env};
 
 mod command;
 use color_eyre::Result;
@@ -6,6 +7,11 @@ use command::{Cli, Command};
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let env = Env::new()
+        .filter("DINOPKG_LOG")
+        .write_style("DINOPKG_LOG_STYLE");
+    env_logger::try_init_from_env(env)?;
+
     let cli = Cli::parse();
     match cli.command {
         Command::Run { script_name } => command::run::run(script_name).await?,
